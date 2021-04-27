58607ced4c1f3b59fce6ef04e5f9887a4ccb5b01
regine-tiu <regine.d.tiu@gmail.com>
2021-04-27 20:31:29 +0800

    refracted once more

--- always_three.rb
+++ always_three.rb
@@ -2,13 +2,4 @@ puts "Give me a number"
 
 first_num = gets.to_i
 
-final_num = first_num
-
-final_num += 5
-final_num *= 2
-final_num -= 4
-final_num /= 2
-
-final_num -= first_num
-
-puts "The final number is always #{final_num}"
+puts "The final number is always " + (((first_num + 5) * 2 - 4) / 2 - first_num).to_s + "."
