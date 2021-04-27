f527466c342c28ab1dbd0bc55f75e1d06b1658b0
regine-tiu <regine.d.tiu@gmail.com>
2021-04-27 19:05:37 +0800

    solved

---
+++ case.rb
@@ -0,0 +1,15 @@
+weather = 'sunny'
+
+case weather
+when "sunny"
+  puts "Don't forget your sunscreen!"
+when "cloudy"
+  puts "Hmm... where did the sun go?"
+when "foggy"
+  puts "Oof! I can barely see a thing"
+when "rainy"
+  puts "Let's go sing in the rain!"
+else
+  puts "Don't forget your sunscreen!"        
+end
+  
