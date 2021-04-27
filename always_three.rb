658493a52580d24f68b3f1a02fd30c9aa6404369
regine-tiu <regine.d.tiu@gmail.com>
2021-04-27 18:42:48 +0800

    solved

---
+++ always_three.rb
@@ -0,0 +1,14 @@
+puts "Give me a number"
+
+first_num = gets.to_i
+
+final_num = first_num
+
+final_num += 5
+final_num *= 2
+final_num -= 4
+final_num /= 2
+
+solution = final_num - first_num
+
+puts "The final number is always #{solution}"
