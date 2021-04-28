04541bd3a989293f0139f4635833e5a2babd671d
regine-tiu <regine.d.tiu@gmail.com>
2021-04-28 15:51:52 +0800

    solved

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(num)
+  (1..num).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end  
+  end  
+end  
+
+fizzbuzz(20)
